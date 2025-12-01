c494c6f7341f9d95445eab155b4afcbd04409795
Minsung Kim <alstjddl0513@gmail.com>
2025-12-01 09:31:57 +0900

    fix-multiply-func

--- calculator.js
+++ calculator.js
@@ -2,9 +2,7 @@
 const add =(a,b)=> a+b;
 
 //type (x :number, y:number) -> number;
-const substract = (a,b) =>a-b;
+const substract = (a,b) => a-b;
 
 //type (x: number, y:number) ->number;
-const multiply = (a, b) =>{
-    return a*b
-}; 
+const multiply = (a, b) => a*b;
